657ff09f2bb86e4aeca6ae33aa2a5670c3c1560c
Aubrey Neimeier <neimeier.1@osu.edu>
2017-11-28 20:40:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
-  elsif board[index] == "X" || board[index] == "O"
+  else
     taken = true
     end
 end
